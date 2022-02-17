5f92af719b17b92b160be6b97099a9356dc382d1
percival banting <percivalbanting@gmail.com>
2022-02-17 16:19:58 +0800

    Student Portal - Clearance Module [02-17-2022]

--- resources/views/student/academic/clearance.blade.php
+++ resources/views/student/academic/clearance.blade.php
@@ -25,7 +25,10 @@ $_title = 'Academic';
                     <h4 class="card-title">E-Clearance</h4>
                 </div>
                 <div class="card-tool">
+                    {{-- {{ Auth::user()->student->clearance_status() }} --}}
                     @if (Auth::user()->student->enrollment_assessment->course_id == 3)
+                        <a href="{{ route('academic.enroll-now') }}" class="btn btn-primary">Enroll Now</a>
+                    @elseif(Auth::user()->student->clearance_status() == 1)
                         <a href="{{ route('academic.enroll-now') }}" class="btn btn-primary">Enroll Now</a>
                     @endif
                 </div>
@@ -73,14 +76,13 @@ $_title = 'Academic';
                                                         @if ($_subject->e_clearance)
                                                             @if ($_subject->e_clearance->is_approved != 1)
                                                                 <span class="text-muted">
-                                                                    <b>{{ $_subject->e_clearance->comment }}</b></span>
+                                                                    <b>{{ $_subject->e_clearance->comments }}</b></span>
                                                             @endif
                                                         @endif
                                                     </td>
                                                     <td></td>
                                                 </tr>
                                             @endif
-
                                         @endforeach
                                     @else
                                         <tr>
@@ -125,7 +127,14 @@ $_title = 'Academic';
                                                         <span class="text-danger">-</span>
                                                     @endif
                                                 </td>
-                                                <td></td>
+                                                <td>
+                                                    @if (Auth::user()->student->non_academic_clearance($_data))
+                                                        @if (Auth::user()->student->non_academic_clearance($_data)->is_approved != 1)
+                                                            <span class="text-muted">
+                                                                <b>{{ Auth::user()->student->non_academic_clearance($_data)->comments }}</b></span>
+                                                        @endif
+                                                    @endif
+                                                </td>
                                                 <td></td>
                                             </tr>
                                         @endforeach
